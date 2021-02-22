684e6ce5a39f83685540a40fe64e55d7f9a07f44
Divyanshu Bhaik <bhaik.divyanshu007@gmail.com>
2021-02-22 12:26:00 +0530

    Add files via upload

---
+++ Previous_Works/Version_1/Segmentation_Final_Models/lung_masking_images.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+
+import numpy as np
+import os
+import cv2
+
+from tqdm import tqdm
+
+from skimage.io import imread, imshow
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+
+
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+
+#########################################loading_Model#################################################
+
+model  = tf.keras.models.load_model('model_lung_mask.h5')
+model.summary()
+
+##############################Saving the Image in size 512x512 and 128x128 pixels#################################
+
+#output path
+output_path = "Outputs/lung_masked_images/"
+orig_size_out_path = output_path + "original_size_512x512/"
+
+resized_out_path = output_path + "resized_128x128/"
+
+resized_colored_out = resized_out_path + "colored/"
+resized_greyscale_out = resized_out_path + "greyscale/"
+
+#input path
+input_images_path_1 = "Dataset_1/Train/ct_scans/"
+input_images_path_2 = "Dataset_1/Test/ct_scans/"
+
+ids_path_1 = next(os.walk(input_images_path_1))[1]
+ids_path_2 = next(os.walk(input_images_path_2))[1]
+
+paths_input = []
+
+for id in ids_path_1:
+    path_1 = input_images_path_1 + id +'/'
+    paths_input.append(path_1)
+
+for id in ids_path_2:
+    path_2 = input_images_path_2 + id +'/'
+    paths_input.append(path_2)
+
+#segmenting and saving the images
+seq = 0
+for n, path in tqdm(enumerate(paths_input), total=len(paths_input)):
+    ids = next(os.walk(path))
+    ids = ids[2]
+    s_im = len(ids)
+    images_128 = np.zeros((s_im, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    images_512 = np.zeros((s_im, 512, 512), dtype=np.uint8)
+    count = 0
+    for img in ids:
+        ip_image = imread(path + img)
+        images_512[count] = ip_image
+        image = resize(ip_image, (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), mode='constant', preserve_range=True)
+        images_128[count] = image
+        count = count+1
+
+    preds = model.predict(images_128, verbose = 1)
+
+    for i in range(len(preds)):
+        #masking the images
+        mask = preds[i]
+        mask = (mask > 0.5).astype(np.uint8)
+        mask_resized = cv2.resize(mask, (512,512), interpolation = cv2.INTER_CUBIC)
+        image_seg = images_128[i]
+        image_seg_resized = images_512[i]
+
+        rows, cols, channels = image_seg.shape
+        rows_resized, cols_resized = image_seg_resized.shape
+
+        image_seg = image_seg[0:rows, 0:cols]
+        image_seg_resized = image_seg_resized[0:rows_resized, 0:cols_resized]
+
+        segmented_image = cv2.bitwise_or(image_seg, image_seg, mask = mask)
+        segmented_image_resized = cv2.bitwise_or(image_seg_resized, image_seg_resized, mask = mask_resized)
+
+        segmented_image = segmented_image[0:rows, 0:cols]
+        segmented_image_resized = segmented_image_resized[0:rows_resized, 0:cols_resized]
+
+        segmented_image_gray = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2GRAY)
+
+        # saving the images
+        place = i+seq
+        cv2.imwrite(orig_size_out_path + "{}.png".format(place), segmented_image_resized)
+        cv2.imwrite(resized_colored_out+"{}.png".format(place),segmented_image)
+        cv2.imwrite(resized_greyscale_out+"{}.png".format(place),segmented_image_gray)
+
+    seq = seq + i
---
+++ Previous_Works/Version_1/Segmentation_Final_Models/training_model_lung_mask.py
@@ -0,0 +1,196 @@
+import tensorflow as tf
+from tensorflow.keras.regularizers import l2
+import numpy as np
+import os
+import random
+
+from tqdm import tqdm
+
+from skimage.io import imread, imshow
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+import time
+
+seed = 99
+np.random.seed = seed
+
+NAME = "D1_lung-mask-{}".format(int(time.time()))
+
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+
+deep_supervision = False
+dropout_rate = 0.5
+num_class = 1
+
+### defining a satndard 2D convolution unit ###
+def standard_unit(input_tensor, stage, nb_filter, kernel_size=3):
+
+    act = 'relu'
+
+    x = tf.keras.layers.Conv2D(nb_filter, (kernel_size, kernel_size), activation = act, name = 'conv'+stage+'_1',
+                        kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = l2(1e-4))(input_tensor)
+    x = tf.keras.layers.Dropout(dropout_rate, name = 'dp'+stage+'_1')(x)
+    x = tf.keras.layers.Conv2D(nb_filter, (kernel_size, kernel_size), activation = act, name = 'conv'+stage+'_2',
+                        kernel_initializer = 'he_normal', padding = 'same', kernel_regularizer = l2(1e-4))(x)
+    x = tf.keras.layers.Dropout(dropout_rate, name = 'dp'+stage+'_2')(x)
+
+    return x
+
+#############################Image_Preprocessing################################################
+
+print("For Train Dataset !")
+
+DATA_TRAIN = "Dataset_1/Train/"
+
+Train_X_path = DATA_TRAIN + "ct_scans/"
+Train_Y_path = DATA_TRAIN + "lung_mask/"
+
+x_ids_train = next(os.walk(Train_X_path))[1]
+y_ids_train = next(os.walk(Train_Y_path))[1]
+
+X_path_train = []
+Y_path_train = []
+
+for id in x_ids_train:
+    path_x = Train_X_path + id +'/'
+    path_y = Train_Y_path + id +'/'
+    X_path_train.append(path_x)
+    Y_path_train.append(path_y)
+
+s_tr = 0
+for n in X_path_train:
+    img_ids = next(os.walk(n))[2]
+    for i in img_ids:
+        s_tr = s_tr+1
+
+
+# important par of inniciating
+X_train = np.zeros((s_tr, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+Y_train = np.zeros((s_tr, IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+
+count_x = 0
+print("Processing for X train data !")
+for n,path in tqdm(enumerate(X_path_train), total=len(X_path_train)):
+    img_ids = next(os.walk(path))[2]
+    for img in img_ids:
+        image = imread(path + img)
+        image = resize(image, (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), mode='constant', preserve_range=True)
+        X_train[count_x] = image
+        count_x = count_x+1
+
+
+count_y = 0
+print("Processing for Y train data !")
+for n,path in tqdm(enumerate(Y_path_train), total=len(Y_path_train)):
+    img_ids = next(os.walk(path))[2]
+    for img in img_ids:
+        image = imread(path + img)
+        image = np.expand_dims(resize(image, (IMG_HEIGHT, IMG_WIDTH), mode='constant',preserve_range=True), axis=-1)
+        Y_train[count_y] = image
+        count_y = count_y+1
+
+
+#################################### making the nested U-net ########################################
+
+
+nb_filter = [32,64,128,256,512]
+act = 'relu'
+
+inputs = tf.keras.layers.Input((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
+s = tf.keras.layers.Lambda(lambda x: x/255)(inputs)
+
+### 1
+conv1_1 = standard_unit(s, stage = '11', nb_filter = nb_filter[0])
+pool1 = tf.keras.layers.MaxPooling2D((2,2), strides = (2,2), name = 'pool1')(conv1_1)
+### 2
+conv2_1 = standard_unit(pool1, stage = '21', nb_filter = nb_filter[1])
+pool2 = tf.keras.layers.MaxPooling2D((2,2), strides = (2,2), name = 'pool2')(conv2_1)
+
+up1_2 = tf.keras.layers.Conv2DTranspose(nb_filter[0], (2,2), strides = (2,2), name = 'up12', padding = 'same')(conv2_1)
+conv1_2 = tf.keras.layers.concatenate([up1_2, conv1_1], name = 'merge12') # check if axis is necessary
+conv1_2 = standard_unit(conv1_2, stage = '12', nb_filter=nb_filter[0])
+### 3
+conv3_1 = standard_unit(pool2, stage = '31', nb_filter = nb_filter[2])
+pool3 = tf.keras.layers.MaxPooling2D((2,2), name = 'pool3')(conv3_1)
+
+up2_2 = tf.keras.layers.Conv2DTranspose(nb_filter[1], (2,2), strides = (2,2), name = 'up22', padding = 'same')(conv3_1)
+conv2_2 = tf.keras.layers.concatenate([up2_2, conv2_1], name = 'merge22') # check for axis
+conv2_2 = standard_unit(conv2_2, stage = '22', nb_filter = nb_filter[1])
+
+up1_3 = tf.keras.layers.Conv2DTranspose(nb_filter[0], (2,2), strides = (2,2), name = 'up13', padding = 'same')(conv2_2)
+conv1_3 = tf.keras.layers.concatenate([up1_3, conv1_1, conv1_2], name = 'merge13') # check for axis
+conv1_3 = standard_unit(conv1_3, stage = '13', nb_filter = nb_filter[0])
+### 4
+conv4_1 = standard_unit(pool3, stage = '41', nb_filter = nb_filter[3])
+pool4 = tf.keras.layers.MaxPooling2D((2,2), strides = (2,2), name='pool4')(conv4_1)
+
+up3_2 = tf.keras.layers.Conv2DTranspose(nb_filter[2], (2,2), strides = (2,2), name = 'up32', padding = 'same')(conv4_1)
+conv3_2 = tf.keras.layers.concatenate([up3_2, conv3_1], name = 'merge32') #check fo axis
+conv3_2 = standard_unit(conv3_2, stage = '32', nb_filter = nb_filter[2])
+
+up2_3 = tf.keras.layers.Conv2DTranspose(nb_filter[1], (2,2), strides = (2,2), name = 'up23', padding = 'same')(conv3_2)
+conv2_3 = tf.keras.layers.concatenate([up2_3, conv2_1, conv2_2], name = 'merge23') # check for axis
+conv2_3 = standard_unit(conv2_3, stage = '23', nb_filter = nb_filter[1])
+
+up1_4 = tf.keras.layers.Conv2DTranspose(nb_filter[0], (2,2), strides = (2,2), name = 'up14', padding = 'same')(conv2_3)
+conv1_4 = tf.keras.layers.concatenate([up1_4, conv1_1, conv1_2, conv1_3], name = 'merge14') # check for axis
+conv1_2 = standard_unit(conv1_4, stage = '14', nb_filter = nb_filter[0])
+### 5
+conv5_1 = standard_unit(pool4, stage='51', nb_filter = nb_filter[4])
+
+up4_2 = tf.keras.layers.Conv2DTranspose(nb_filter[3], (2,2), strides = (2,2), name = 'up42', padding = 'same')(conv5_1)
+conv4_2 = tf.keras.layers.concatenate([up4_2, conv4_1], name='merge42') # check for axis
+conv4_2 = standard_unit(conv4_2, stage = '42', nb_filter = nb_filter[3])
+
+up3_3 = tf.keras.layers.Conv2DTranspose(nb_filter[2], (2,2), strides = (2,2), name = 'up33', padding = 'same')(conv4_2)
+conv3_3 = tf.keras.layers.concatenate([up3_3, conv3_1, conv3_2], name = 'merge33') # check for axis
+conv3_3 = standard_unit(conv3_3, stage = '33', nb_filter = nb_filter[2])
+
+up2_4 = tf.keras.layers.Conv2DTranspose(nb_filter[1], (2,2), strides = (2,2), name = 'up24', padding = 'same')(conv3_3)
+conv2_4 = tf.keras.layers.concatenate([up2_4, conv2_1, conv2_2, conv2_3], name = 'merge24') # check for axis
+conv2_4 = standard_unit(conv2_4, stage = '24', nb_filter = nb_filter[1])
+
+up1_5 = tf.keras.layers.Conv2DTranspose(nb_filter[0], (2,2), strides = (2,2), name = 'up15', padding = 'same')(conv2_4)
+conv1_5 = tf.keras.layers.concatenate([up1_5, conv1_1, conv1_2, conv1_3, conv1_4], name = 'merge15') # check for axis
+conv1_5 = standard_unit(conv1_5, stage = '15', nb_filter = nb_filter[0])
+### end
+
+nested_output_1 = tf.keras.layers.Conv2D(num_class, (1,1), activation = 'sigmoid', name = 'output_1', kernel_initializer = 'he_normal',
+                                    padding = 'same', kernel_regularizer = l2(1e-4))(conv1_2)
+nested_output_2 = tf.keras.layers.Conv2D(num_class, (1,1), activation = 'sigmoid', name = 'output_2', kernel_initializer = 'he_normal',
+                                    padding = 'same', kernel_regularizer = l2(1e-4))(conv1_3)
+nested_output_3 = tf.keras.layers.Conv2D(num_class, (1,1), activation = 'sigmoid', name = 'output_3', kernel_initializer = 'he_normal',
+                                    padding = 'same', kernel_regularizer = l2(1e-4))(conv1_4)
+nested_output_4 = tf.keras.layers.Conv2D(num_class, (1,1), activation = 'sigmoid', name = 'output_4', kernel_initializer = 'he_normal',
+                                    padding = 'same', kernel_regularizer = l2(1e-4))(conv1_5)
+
+
+if deep_supervision:
+    model = tf.keras.Model(inputs = [inputs], outputs = [nested_output_1,
+                                                        nested_output_2,
+                                                        nested_output_3,
+                                                        nested_output_4])
+else:
+    model = tf.keras.Model(inputs = [inputs], outputs = [nested_output_4])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
+
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+    tf.keras.callbacks.TensorBoard(log_dir='logs\{}'.format(NAME)),
+    tf.keras.callbacks.ModelCheckpoint('model_lung_mask.h5', verbose=1, save_best_only=True)]
+
+results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=10, epochs=25, callbacks=callbacks)
+
+
+
+# to run the tensorboard command
+# tensorboard --logdir=logs/
+# now run the command and the oprn the browser
+# you will get a local host address copy that address
+# in the browser address bar type following:
+# pat the copied address
+# and press enter
